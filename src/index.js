ee18189b3f97a1d1b76f8eced238af18f9c3a6c0
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-10-24 19:50:32 +0300

    Fix Leaflet icon not being visible

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import L from 'leaflet';
+
+//Fix Leaflet default icon not being visible
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
