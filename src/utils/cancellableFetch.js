b1ab32ce65e7b7e9ed87fafc962e8ebd509107d1
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-10-24 19:48:24 +0300

    Create cancellable fetch function

---
+++ src/utils/cancellableFetch.js
@@ -0,0 +1,12 @@
+export default (url, options) => {
+  if ('AbortController' in window) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    return {
+      result: fetch(url, { ...options, signal }),
+      cancel: () => controller.abort(),
+    };
+  } else {
+    return { result: fetch(url, options), cancel: () => {} };
+  }
+};
