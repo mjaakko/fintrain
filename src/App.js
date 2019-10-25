135c8755d7494ee34960f95297cc974674153302
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-10-25 18:52:13 +0300

    Use react-router for navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import FrontPage from './components/pages/FrontPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default () => {
-  return <FrontPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <FrontPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
