3cead697fdc1c1456e0ed4ffbc6152c7a998f954
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-10-29 20:27:38 +0200

    Create reducer to use in hooks that load data

---
+++ src/reducers/loaderReducer.js
@@ -0,0 +1,12 @@
+export default (state, action) => {
+  switch (action.type) {
+    case 'loading':
+      return { loading: true, data: null, error: null };
+    case 'data':
+      return { loading: false, data: action.data, error: null };
+    case 'error':
+      return { loading: false, data: null, error: action.error };
+    default:
+      throw new Error('Invalid action type');
+  }
+};
