4fd95fd34315e9e0f469b2f839e15ebac680e6db
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-10-31 18:25:15 +0200

    Create React hook for using geolocation

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default () => {
+  const [status, setStatus] = useState();
+  const [position, setPosition] = useState();
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      setStatus({ status: 'WATCHING', error: null });
+      const watchId = navigator.geolocation.watchPosition(
+        position => {
+          setPosition(position);
+        },
+        error => {
+          setStatus({ status: 'ERROR', error });
+        }
+      );
+
+      return () => navigator.geolocation.clearWatch(watchId);
+    } else {
+      setStatus({ status: 'UNAVAILABLE', error: null });
+    }
+  }, []);
+
+  return { position, status };
+};
