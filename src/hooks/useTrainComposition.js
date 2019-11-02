bb77859c65fdaca734a27ad6f2f65df129a6a31c
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-02 09:12:09 +0200

    Create React hook for using train composition

---
+++ src/hooks/useTrainComposition.js
@@ -0,0 +1,30 @@
+import { useReducer, useEffect } from 'react';
+
+import loaderReducer from '../reducers/loaderReducer';
+
+import { getTrainComposition } from '../services/rataDigitrafficService';
+
+export default (trainNumber, departureDate) => {
+  const [state, dispatch] = useReducer(loaderReducer, {
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    const { result, cancel } = getTrainComposition(trainNumber, departureDate);
+    result
+      .then(trainComposition =>
+        dispatch({ type: 'data', data: trainComposition })
+      )
+      .catch(error => dispatch({ type: 'error', error }));
+
+    return () => cancel();
+  }, [trainNumber, departureDate]);
+
+  return {
+    loading: state.loading,
+    trainComposition: state.data,
+    error: state.error,
+  };
+};
