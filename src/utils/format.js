a918bf7a014ae74df09b203f874f2373a97ae95e
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-07 20:05:11 +0200

    Remove useless console.log

--- src/utils/format.js
+++ src/utils/format.js
@@ -8,8 +8,6 @@ export const formatTime = time => moment(time).format('HH:mm');
 export const formatStationName = stationName => {
   const formattedStationName = stationName.replace('_', ' ');
 
-  console.log(stationName);
-
   return formattedStationName.endsWith(' asema')
     ? formattedStationName.substring(0, stationName.indexOf(' asema'))
     : formattedStationName;
