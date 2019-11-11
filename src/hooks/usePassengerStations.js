d6751389d8ded8393f31fd7492074c1ac1d73671
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-11 20:01:57 +0200

    Use hardcoded location for Russian stations which have incorrect coordinates in rata.digitraffic.fi APIs

--- src/hooks/usePassengerStations.js
+++ src/hooks/usePassengerStations.js
@@ -4,6 +4,22 @@ import loaderReducer from '../reducers/loaderReducer';
 
 import { getPassengerStations } from '../services/rataDigitrafficService';
 
+//Russian stations have incorrect coordinates in the API
+const RUSSIAN_STATIONS = {
+  VYB: {
+    latitude: 60.71585,
+    longitude: 28.75142,
+  },
+  PTR: {
+    latitude: 59.95601,
+    longitude: 30.35704,
+  },
+  MVA: {
+    latitude: 55.77789,
+    longitude: 37.65535,
+  },
+};
+
 export default () => {
   const [state, dispatch] = useReducer(loaderReducer, {
     loading: true,
@@ -16,10 +32,16 @@ export default () => {
       .then(stations =>
         dispatch({
           type: 'data',
-          data: stations.reduce(
-            (map, station) => map.set(station.stationShortCode, station),
-            new Map()
-          ),
+          data: stations
+            .map(station => {
+              return station.stationShortCode in RUSSIAN_STATIONS
+                ? { ...station, ...RUSSIAN_STATIONS[station.stationShortCode] }
+                : station;
+            })
+            .reduce(
+              (map, station) => map.set(station.stationShortCode, station),
+              new Map()
+            ),
         })
       )
       .catch(error => dispatch({ type: 'error', error }));
