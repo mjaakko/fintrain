e5fc74bde853ff807f69d5610f1b5e66636084e9
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2019-11-14 21:33:43 +0200

    Display time delayed and reason for delay in popup

--- src/components/TrainTime.js
+++ src/components/TrainTime.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Popup } from 'semantic-ui-react';
+
+import { MetadataContext } from '../App';
 
 import { formatTime } from '../utils/format';
 
@@ -9,7 +11,33 @@ const MAJOR_DELAY = 15;
 const TIMEZONE_FINLAND = 'Europe/Helsinki';
 const TIMEZONE_RUSSIA = 'Europe/Moscow'; //In Russia trains run on Moscow time
 
+const generateCausesString = (detailedCauseCodes, detailedCauses) => {
+  if (!detailedCauses) {
+    return null;
+  }
+  const passengerTerms = detailedCauseCodes
+    .map(detailedCauseCode => detailedCauses.get(detailedCauseCode))
+    .map(detailedCause => {
+      if (
+        !detailedCause ||
+        !detailedCause.passengerTerm ||
+        !detailedCause.passengerTerm.en
+      ) {
+        return null;
+      }
+      return detailedCause.passengerTerm.en.toLocaleLowerCase('en');
+    })
+    .filter(x => !!x);
+  return passengerTerms.length !== 0
+    ? passengerTerms.reduce((acc, cur, index, { length }) => {
+        return acc + (index === length - 1 ? ' and ' : ', ') + cur;
+      })
+    : null;
+};
+
 export default ({ timetableRow }) => {
+  const { detailedCauses } = useContext(MetadataContext);
+
   if (!timetableRow) {
     return <time>--</time>;
   }
@@ -18,35 +46,45 @@ export default ({ timetableRow }) => {
     timetableRow.actualTime ||
     timetableRow.liveEstimateTime ||
     timetableRow.scheduledTime;
-  const timeElement = (
-    <time
-      className={`${timetableRow.actualTime ? 'actual' : ''} ${
-        timetableRow.differenceInMinutes
-          ? timetableRow.differenceInMinutes <= MINOR_DELAY
-            ? 'ontime'
-            : timetableRow.differenceInMinutes <= MAJOR_DELAY
-            ? 'minordelay'
-            : 'majordelay'
-          : ''
-      }`}
-      dateTime={time}
-    >
-      {formatTime(
-        time,
-        timetableRow.countryCode === 'RU' ? TIMEZONE_RUSSIA : TIMEZONE_FINLAND
-      )}
-      {timetableRow.unknownDelay ? '\u00a0(?)' : ''}
-    </time>
+
+  const causes = generateCausesString(
+    timetableRow.causes.map(cause => cause.detailedCategoryCode),
+    detailedCauses
   );
 
-  return timetableRow.unknownDelay > 0 ? (
+  return (
     <Popup
-      content={`Exact ${
-        timetableRow.type === 'ARRIVAL' ? 'arrival' : 'departure'
-      } time is unknown`}
-      trigger={timeElement}
+      positionFixed
+      content={`Delayed ${
+        timetableRow.unknownDelay
+          ? 'by unknown time'
+          : `${timetableRow.differenceInMinutes}min`
+      }${causes ? ` due to ${causes}` : ''}`}
+      disabled={
+        !(timetableRow.unknownDelay || timetableRow.differenceInMinutes > 0)
+      }
+      trigger={
+        <time
+          className={`${timetableRow.actualTime ? 'actual' : ''} ${
+            timetableRow.differenceInMinutes
+              ? timetableRow.differenceInMinutes <= MINOR_DELAY
+                ? 'ontime'
+                : timetableRow.differenceInMinutes <= MAJOR_DELAY
+                ? 'minordelay'
+                : 'majordelay'
+              : ''
+          }`}
+          dateTime={time}
+        >
+          {formatTime(
+            time,
+            timetableRow.countryCode === 'RU'
+              ? TIMEZONE_RUSSIA
+              : TIMEZONE_FINLAND
+          )}
+          {timetableRow.unknownDelay && '\u00a0?'}
+        </time>
+      }
     />
-  ) : (
-    timeElement
   );
 };
