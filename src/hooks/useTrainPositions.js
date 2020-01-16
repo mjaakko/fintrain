c1e4763f625bac15f72a07235b2220fe39504c83
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2020-01-16 20:11:49 +0200

    Subscribe to each train only once

--- src/hooks/useTrainPositions.js
+++ src/hooks/useTrainPositions.js
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useRef } from 'react';
 
 import useMqttClient from './useMqttClient';
 
@@ -11,31 +11,30 @@ const options = {
 };
 
 export default (maxAgeSecs, trains) => {
-  const [trainPositions, dispatch] = useReducer(
-    (state, action) => {
-      switch (action.type) {
-        case 'update_train_position':
-          return {
-            ...state,
-            [`${action.trainPosition.trainNumber}-${action.trainPosition.departureDate}`]: action.trainPosition,
-          };
-        case 'delete_old_entries':
-          const now = new Date();
-          return Object.keys(state).reduce((result, key) => {
-            if (now - new Date(state[key].timestamp) < maxAgeSecs * 1000) {
-              result[key] = state[key];
-            }
+  const [trainPositions, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'update_train_position':
+        return {
+          ...state,
+          [`${action.trainPosition.trainNumber}-${action.trainPosition.departureDate}`]: action.trainPosition,
+        };
+      case 'delete_old_entries':
+        const now = new Date();
+        return Object.keys(state).reduce((result, key) => {
+          if (now - new Date(state[key].timestamp) < maxAgeSecs * 1000) {
+            result[key] = state[key];
+          }
 
-            return result;
-          }, {});
-        default:
-          return state;
-      }
-    },
-    { trainPositions: {}, error: null }
-  );
+          return result;
+        }, {});
+      default:
+        return state;
+    }
+  }, {});
 
   const { client, error } = useMqttClient(endpoint, options);
+
+  const subscribedTrains = useRef(new Set());
 
   useEffect(() => {
     if (client) {
@@ -48,12 +47,39 @@ export default (maxAgeSecs, trains) => {
   useEffect(() => {
     if (client) {
       if (Array.isArray(trains) && trains.length > 0) {
-        trains.forEach(train =>
-          client.subscribe(
-            `train-locations/${train.departureDate}/${train.trainNumber}/#`
-          )
+        if (subscribedTrains.current === null) {
+          subscribedTrains.current = new Set();
+          client.unsubscribe('train-locations/#');
+        }
+
+        trains.forEach(train => {
+          //Subscribe each train only once
+          if (
+            !subscribedTrains.current.has(
+              `${train.departureDate}/${train.trainNumber}`
+            )
+          ) {
+            subscribedTrains.current.add(
+              `${train.departureDate}/${train.trainNumber}`
+            );
+            client.subscribe(
+              `train-locations/${train.departureDate}/${train.trainNumber}/#`
+            );
+          }
+        });
+
+        //Unsubscribe previously subscribed trains
+        const currentSubscription = new Set(
+          trains.map(train => `${train.departureDate}/${train.trainNumber}`)
         );
-      } else {
+        subscribedTrains.current.forEach(subscribedTrain => {
+          if (!currentSubscription.has(subscribedTrain)) {
+            subscribedTrains.current.delete(subscribedTrain);
+            client.unsubscribe(`train-locations/${subscribedTrain}/#`);
+          }
+        });
+      } else if (trains === undefined) {
+        subscribedTrains.current = null;
         client.subscribe('train-locations/#');
       }
     }
