9595f9e284293433b3298bceedfa806aa8963f34
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2020-03-02 19:32:31 +0200

    Open station popup based on MapContexxt

--- src/components/pages/FrontPage/StationMarkers.js
+++ src/components/pages/FrontPage/StationMarkers.js
@@ -1,17 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 
 import { MetadataContext } from '../../../App';
+import { MapContext } from './FrontPage';
 
 import StationName from '../../StationName';
 
 const StationMarker = ({
   station: { latitude, longitude, stationShortCode },
 }) => {
+  const { activePopup, setActivePopup } = useContext(MapContext);
+
+  const popupRef = useCallback(
+    node => {
+      if (
+        node !== null &&
+        node.leafletElement !== null &&
+        activePopup &&
+        activePopup.type === 'STATION' &&
+        activePopup.code === stationShortCode &&
+        !node.leafletElement.isPopupOpen()
+      ) {
+        node.leafletElement.openPopup();
+      }
+    },
+    [activePopup]
+  );
+
   return (
-    <Marker position={{ lat: latitude, lng: longitude }}>
-      <Popup>
+    <Marker ref={popupRef} position={{ lat: latitude, lng: longitude }}>
+      <Popup
+        onOpen={() =>
+          setActivePopup({ type: 'STATION', code: stationShortCode })
+        }
+        onClose={() => setActivePopup(null)}
+      >
         <Link to={`/station/${stationShortCode}`}>
           <StationName stationShortCode={stationShortCode} />
         </Link>
